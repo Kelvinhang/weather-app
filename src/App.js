dcc01ad6b59835fc3c961a823390ac969ccf880d
Kelvinhang <hfyj77h47@gmail.com>
2022-11-26 17:52:38 +0800

    add template and style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <>
-    </>
+    <div className="weatherInfo">
+      <div className="city">Hong Kong</div>
+      <div className="date">26/11/2022</div>
+      <div className="time">17:36</div>
+      <div className="temp">24°C</div>
+    </div>
   );
 }
 
